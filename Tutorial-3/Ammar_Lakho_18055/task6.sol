c6f65973824e5815ac7faeccfe5ec6f261337555
ammarlakho <ammar1lakho@gmail.com>
2022-03-13 19:01:56 +0500

    Ammar Lakho done

---
+++ Tutorial-3/Ammar_Lakho_18055/task6.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: GPL-3.0
+ 
+pragma solidity >=0.5.0 <0.9.0;
+ 
+contract A {
+    int public x = 10;
+    
+    function f3() internal view returns (int) {
+        return x;
+    }
+    
+}
+
+
+contract B is A {
+
+    function f4() public view returns (int) {
+        return f3();
+    }
+}
